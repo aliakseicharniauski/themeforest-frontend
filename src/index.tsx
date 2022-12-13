36bb7484c0060f82d2072a722a2148bd29f7eafd
aliaksei.charniauski <aliaksei.charniauski@yellow.systems>
2022-12-13 13:55:34 +0300

    add prettier eslint-prettier prettierignore

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Hello } from "./components/Hello";
 
-const App = () => <h1>Themeforest Modsen init</h1>;
+const App = () => (
+  <h1>
+    Themeforest Modsen init
+    <Hello compiler="compiler" framework="framework" />
+  </h1>
+);
 
 ReactDOM.render(
   <React.StrictMode>
