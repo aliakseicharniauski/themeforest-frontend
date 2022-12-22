d81db057c22f084bd03108afcf8e5c287e2a4e06
aliaksei.charniauski <aliaksei.charniauski@yellow.systems>
2022-12-23 01:44:04 +0300

    add lint-staged

--- custom.d.ts
+++ custom.d.ts
@@ -1,6 +1,8 @@
-declare module "*.svg" {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-  export default content;
+import React from 'react'
+
+declare module '*.svg' {
+    const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
+    export default content
 }
 
-declare module "*.ttf";
+declare module '*.ttf'
