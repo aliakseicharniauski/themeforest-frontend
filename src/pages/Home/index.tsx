f823675cf86adbe192ae02efa851a0550d0e1c93
aliaksei.charniauski <aliaksei.charniauski@modsen-software.com>
2022-12-30 00:28:42 +0300

    add sample custom package importing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,39 +1,43 @@
 import React from 'react'
 
+import { Button, Icon } from '@aliakseicharniauski/themeforest-library'
+
 const Home: React.FC = () => (
-        <div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-            <div>home home home</div>
-        </div>
-    )
+    <div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <div>home home home</div>
+        <Button startIcon={<Icon.IconArrowLeft />} label="test button" />
+        <Icon.IconBookDatabase />
+    </div>
+)
 
 export default Home
